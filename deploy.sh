5612c3d166b30f53de23747e3889ee5434090f4f
javajigi <javajigi@gmail.com>
2017-07-06 18:12:05 +0900

    refactor: deploy shell

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+pkill -9 -f jwp-trello
+
 ./gradlew clean build
 echo "build successful!"
-
-pkill -9 -f jwp-trello
 
 cd build/libs
 
 java -Dserver.port=8000 -jar jwp-trello-1.0.0.jar >> trello.log 2>&1 &
+
+echo "started server!"
 
